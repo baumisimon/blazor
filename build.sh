03c3302ce2d3313ae42bbf764c42d163d98f3638
Simon Baumgartner <simon.baumgartner@maut1.de>
2021-12-01 20:58:09 +0100

    Adjust build script

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSLO https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --channel LTS --install-dir ./dotnetLatest
 ./dotnetLatest/dotnet --version
-./dotnetLatest/dotnet publish -c Release -o output
+./dotnetLatest/dotnet publish -c Release
