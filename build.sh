9a7e0cb1ac6c902956485c230497bb3ee24634e3
Simon Baumgartner <simon.baumgartner@maut1.de>
2021-12-01 22:02:51 +0100

    manual install of dotnet

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-curl -sSLO https://dot.net/v1/dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh --channel LTS --install-dir ./dotnetLatest
+curl -sSLO https://download.visualstudio.microsoft.com/download/pr/17b6759f-1af0-41bc-ab12-209ba0377779/e8d02195dbf1434b940e0f05ae086453/dotnet-sdk-6.0.100-linux-x64.tar.gz
+
+DOTNET_FILE=dotnet-sdk-6.0.100-linux-x64.tar.gz
+export DOTNET_ROOT=$(pwd)/dotnetLatest
+mkdir -p "$DOTNET_ROOT" && tar zxf "$DOTNET_FILE" -C "$DOTNET_ROOT"
+export PATH=$PATH:$DOTNET_ROOT
+
 ./dotnetLatest/dotnet --version
-export PATH="$PATH:$PWD/dotnetLatest"
-echo $PATH
 ./dotnetLatest/dotnet publish -c Release -o output
