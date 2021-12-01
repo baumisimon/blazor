2e3fdbf812f9cb7a572beb0a38b5abd125ee3146
Simon Baumgartner <simon.baumgartner@maut1.de>
2021-12-01 21:33:30 +0100

    Adjust dotnet path

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ curl -sSLO https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --channel LTS --install-dir ./dotnetLatest
 ./dotnetLatest/dotnet --version
-export PATH="$PATH:$PWD/dotnetLatest/sdk/6.0.100/Sdks"
+export PATH="$PATH:$PWD/dotnetLatest"
 echo $PATH
 ./dotnetLatest/dotnet publish -c Release -o output
